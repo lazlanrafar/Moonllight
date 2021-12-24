7500c0dbd213a8e0bc1924315c28b83ee952aaf9
lazlanrafar <lazlanrafar@gmail.com>
2021-12-24 07:55:28 +0700

    add background gradient

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <a href="" class="logo">Logo</a>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Package</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/navbar.scss";
+</style>
